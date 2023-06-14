1cb1b5425fd45606c34c104d2d6c3132bc6b619a
Vladimir <sviderskijvladimir@gmail.com>
2023-06-14 21:04:53 +0300

    create list of products

--- amazona/frontend/src/App.js
+++ amazona/frontend/src/App.js
@@ -1,10 +1,32 @@
+import data from "./data";
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">amazona</a>
       </header>
-      <main>list products</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((p) => (
+            <div className="product" key={p.slug}>
+              <a href={`/product/${p.slug}`}>
+                <img src={p.image} alt={p.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${p.slug}`}>
+                  <p>{p.name}</p>
+                </a>
+                <p>
+                  <strong>${p.price}</strong>
+                </p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
