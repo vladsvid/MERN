0281bb4858da6e0bcfecd5867f32c2e89cc70906
Vladimir <sviderskijvladimir@gmail.com>
2023-06-27 14:42:05 +0300

    create sample users

--- amazona/backend/routes/seedRoutes.js
+++ amazona/backend/routes/seedRoutes.js
@@ -1,13 +1,15 @@
 import express from "express";
 import Product from "../models/productModel.js";
 import data from "../data.js";
+import User from "../models/userModel.js";
 
 const seedRoutes = express.Router();
 
 seedRoutes.get("/", async (req, res) => {
-  // await Product.remove({});
+  // await Product.remove();
   const createdProducts = await Product.insertMany(data.products);
-  res.send({ createdProducts });
+  const createdUsers = await User.insertMany(data.users);
+  res.send({ createdProducts, createdUsers });
 });
 
 export default seedRoutes;
